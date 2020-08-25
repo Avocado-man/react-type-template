65930ebc847298fc446a6f482c67ec5f959f8b0b
Andrii Otroshenko <65036633+Avocado-man@users.noreply.github.com>
2020-08-25 12:40:49 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './App.scss'
 
 const App: React.FC = (): JSX.Element => {
-    return <div className="App" />
+    return (
+        <div className="App">
+            <h1>Hello React!</h1>
+        </div>
+    )
 }
 
 export default App
